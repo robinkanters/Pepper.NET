a682ea25cc57826406ef37e118f3e0d5720412b3
Robin Kanters <robin@robinkanters.nl>
2015-09-05 20:31:36 +0200

    Created project for tests

---
+++ Pepper.NET.Tests/Model/ProfileTest.cs
@@ -0,0 +1,12 @@
+﻿namespace Pepper.NET.Tests
+{
+    using NUnit.Framework;
+    
+    public class ProfileTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
